b6d4b383939798bd89d5f001a8786e64b923e075
ElaineHuang <starbrave1313677@gmail.com>
2018-01-14 21:34:36 +0800

    remove unnecessary pkg

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 const serverConfig     = require('./configs/server-config');
 const express = require('express');
 const app = express();
-const history = require('express-history-api-fallback');
 const path = require('path');
+const root = path.resolve(__dirname, 'build');
 
-if (process.env.NODE_DEV !== 'production') {
-  const webpackMiddleware = require('webpack-dev-middleware');
-  const webpack = require('webpack');
-  const webpackConfig = require('./configs/webpack.config.js');
-  app.use(webpackMiddleware(webpack(webpackConfig)));
-} else {
-  const root = path.resolve(__dirname, 'build');
-  app.use(express.static(root));
-  app.use(history('index.html', { root }));
-}
+app.use(express.static(root));
+app.get('*', function(req, res) {
+  res.sendfile(path.resolve(root, 'index.html'));
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('node development mode now is listening on:');
