11048db951d2d01e6a3be67dc25bbfc5b14ec2c4
ElaineHuang <starbrave1313677@gmail.com>
2018-01-15 23:57:07 +0800

    add Header css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,14 @@ import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
 import { Link } from 'react-router';
 import PATHS from 'constants/paths';
+import styles from './header.scss';
 
 export default class Header extends PureComponent {
   render () {
     return (
-      <div className="header">
-        Header
-        <Link to={PATHS.ABOUT}>About</Link>
+      <div className={styles.header}>
+        <Link className={styles[this.props.pathname === PATHS.ROOT ? 'link-active' : 'link']} to={PATHS.ROOT}>Home</Link>
+        <Link className={styles[this.props.pathname === PATHS.ABOUT ? 'link-active' : 'link']} to={PATHS.ABOUT}>About</Link>
       </div>
     );
   }
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -3,10 +3,11 @@ import React, { PureComponent } from 'react';
 import { Header, Footer } from 'components';
 
 export default class Main extends PureComponent {
+
   render () {
     return (
       <div>
-        <Header />
+        <Header pathname={this.props.location.pathname} />
         {this.props.children}
         <Footer />
       </div>
