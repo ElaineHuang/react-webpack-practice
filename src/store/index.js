556a02423b79b78237d3ae583c8efc66a5b278bb
ElaineHuang <starbrave1313677@gmail.com>
2018-01-30 22:31:11 +0800

    add redux-devtool

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import {
   createStore,
   applyMiddleware,
 } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { syncHistoryWithStore } from 'react-router-redux';
 import rootReducer from 'reducers';
 import rootSaga from 'sagas';
@@ -11,7 +12,14 @@ import {
   saga,
 } from './middlewares';
 
-const store = createStore(rootReducer, {}, applyMiddleware(saga, router));
+const middlewareEnhancer = (() => {
+  if (process.env.NODE_ENV === 'development') {
+    return composeWithDevTools(applyMiddleware(saga, router));
+  }
+  return applyMiddleware(saga, router);
+})();
+
+const store = createStore(rootReducer, middlewareEnhancer);
 const history = syncHistoryWithStore(immatureHistory, store);
 
 saga.run(rootSaga);
