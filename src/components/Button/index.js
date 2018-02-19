9d98b0999380031686704891b295ca5a4e192291
ElaineHuang <starbrave1313677@gmail.com>
2018-02-19 23:40:32 +0800

    Refactor to Functional stateless component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,33 +3,23 @@ import React, { PureComponent } from 'react';
 import styles from './button.scss';
 import cx from 'classnames';
 
-export default class Dialog extends PureComponent {
+const Button = ({className = 'btn', onClick, children = null, type = 'button'}) => {
+  return (
+    <button
+      className={styles[className]}
+      onClick={onClick}
+      type={type}
+    >
+      {children}
+    </button>
+  );
+};
 
-  static propTypes = {
-    className: PropTypes.string,
-    onClick: PropTypes.func,
-    children: PropTypes.any,
-    type: PropTypes.string,
-  };
+Button.propTypes = {
+  className: PropTypes.string,
+  onClick: PropTypes.func,
+  children: PropTypes.any,
+  type: PropTypes.string,
+};
 
-  static defaultProps = {
-    className: 'btn',
-    onClick: () => console.log('button'),
-    children: null,
-    type: 'button',
-  };
-
-  render () {
-    const { className, onClick, children, type } = this.props;
-
-    return (
-      <button
-        className={styles[className]}
-        onClick={onClick}
-        type={type}
-      >
-        {children}
-      </button>
-    );
-  }
-}
+export default Button;
